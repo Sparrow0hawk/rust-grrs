0782392ae0dc0414e86fbc1b6263d9d8306bf7a6
sparrow0hawk <alexjcoleman@gmail.com>
2020-10-17 18:51:15 +0100

    using structopt to generate cli args

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,16 @@
+use structopt::StructOpt;
+
+// search for a pattern in a file and display the lines that contain it.
+#[derive(StructOpt)]
 struct Cli {
+    // the pattern to look for
     pattern: String,
+    // the path to the files to read
+    #[structopt(parse(from_os_str))]
     path: std::path::PathBuf,
 }
 
 // main function
 fn main() {
-    println!("Hello, world!");
-
-    let pattern = std::env::args().nth(1).expect("no pattern given");
-    let path = std::env::args().nth(2).expect("no path given");
-    let args = Cli {
-        pattern: pattern,
-        path: std::path::PathBuf::from(path)
-    };
+    let args = Cli::from_args();
 }
