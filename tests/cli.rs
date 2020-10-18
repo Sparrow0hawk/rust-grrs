d42a349fa4c742c8f72ce11ba7ce4896a667ab2c
sparrow0hawk <alexjcoleman@gmail.com>
2020-10-18 19:03:40 +0100

    integration test ideas

---
+++ tests/cli.rs
@@ -0,0 +1,15 @@
+use assert_cmd::prelude::*;
+use predicates::prelude::*;
+use std::process::Command;
+
+#[test]
+fn file_doesnt_exist() -> Result<(), Box<dyn std::error::Error>> {
+    let mut cmd = Command::cargo_bin("grrs")?;
+
+    cmd.arg("foobar").arg("directory/that/isnt/here");
+    cmd.assert()
+        .failure()
+        .stderr(predicate::str::contains("No such file or directory"));
+
+    Ok(())
+}
